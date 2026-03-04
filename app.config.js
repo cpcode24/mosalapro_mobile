83d5caaed6f3dee5d76697c85842fcff883aeadb
Constant Pagoui <constantpagoui@gmail.com>
2026-03-03 20:23:35 -0500

    Updated config

--- app.config.js
+++ app.config.js
@@ -1,17 +1,40 @@
 export default {
-  name: "mosalapro-mobile",
-  displayName: "MosalaPro Mobile",
-  platforms: ["ios", "android", "web"],
-  web: {
-    bundler: "metro"
-  },
-  metro: {
-    resolver: {
-      alias: {
-        "react-native-pager-view": "./src/components/PagerViewWrapper.js"
-      },
-      platforms: ["ios", "android", "web"]
-    }
-  },
-  jsEngine: "hermes"
+  expo: {
+    name: "MosalaPro Mobile",
+    slug: "mosalapro-mobile",
+    version: "1.0.0",
+    platforms: ["ios", "android", "web"],
+    android: {
+      package: "com.mosalapro.mobile"
+    },
+    ios: {
+      bundleIdentifier: "com.mosalapro.mobile",
+      supportsTablet: true,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    plugins: [
+      [
+        "@stripe/stripe-react-native",
+        {
+          merchantIdentifier: "merchant.com.mosalapro.mobile",
+          enableGooglePay: true
+        }
+      ]
+    ],
+    web: {
+      bundler: "metro"
+    },
+    jsEngine: "hermes",
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    extra: {
+      eas: {
+        projectId: "6f5821f6-7e51-45fc-ac95-6f65eb28c490"
+      }
+    },
+    orientation: "portrait"
+  }
 };
